c51beb744a7a0725b33fdf2e3e3290da386ab568
toma <toma95s@tomas-MBP.lan>
2022-05-23 22:21:24 -0700

    added user.js new schema,

---
+++ Models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose'); 
+const moment = require('moment');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+
+
+        thoughts:
+            [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+
+
+        friends:
+            [{ type: Schema.Types.ObjectId, ref: 'User' }],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
